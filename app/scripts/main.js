1e1fd3b7dc2b92ed61d3141c3eafa0b742011b67
Gloryofthe80s <gloryoftheeighties@gmail.com>
2014-01-30 20:46:12 -0500

    got cancel button working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -27,25 +27,29 @@ $('.compose-btn').click(function() {
     $('.shadow-toggle').addClass('active');
     $('body').append(modalTemplate());
 
+    // if cancel is clicked
+    $('.cancel-btn').click(function() {
+        $('.shadow-toggle').removeClass('active');
+        $('.compose-box').remove();
+    });
+
     // create new task
     $('.submit-btn').click(function() {
-    $('.shadow-toggle').removeClass('active');
+        $('.shadow-toggle').removeClass('active');
 
-    var input = {
-        task: $('.task-input').val(),
-        dueDate: $('.due-date-input').val(),
-        notes: $('.notes-input').val()
-    };
+        var input = {
+            task: $('.task-input').val(),
+            dueDate: $('.due-date-input').val(),
+            notes: $('.notes-input').val()
+        };
 
     taskArray.push(new TaskObject(input));
 
     $('.compose-box').remove();
 
     _.each(taskArray, function(task, index) {
-    $('.container').append(taskTemplate(task));
+        $('.container').append(taskTemplate(task));
     });
-
-
     });
 });
 
